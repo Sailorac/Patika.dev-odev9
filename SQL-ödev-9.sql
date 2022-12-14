b5a52161ddfd803cc29b2037440fc0d17c9041f5
Sailorac <119450113+Sailorac@users.noreply.github.com>
2022-12-14 21:30:30 +0300

    Add files via upload

---
+++ SQL-ödev-9.sql
@@ -0,0 +1,10 @@
+--select city,country from city
+--inner join country on city.country_id = country.country_id;
+
+
+--select payment_id,first_name,last_name from customer
+--inner join payment ON payment.customer_id = customer.customer_id
+
+
+select rental_id,first_name,last_name from customer
+inner join rental ON rental.customer_id = customer.customer_id;
